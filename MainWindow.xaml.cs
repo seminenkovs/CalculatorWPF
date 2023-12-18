b1eb87a8385c363ad46cfc366e150cbc149f5a74
Vitalii <seminenkovs@gmail.com>
2023-12-18 02:23:40 +0100

    Division by 0 fixed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -145,7 +145,17 @@ namespace CalculatorWPF
         public static double Add(double n1, double n2) => n1 + n2;
         public static double Substract(double n1, double n2) => n1 - n2;
         public static double Multiply(double n1, double n2) => n1 * n2;
-        public static double Divide(double n1, double n2) => n1 / n2;
+
+        public static double Divide(double n1, double n2)
+        {
+            if (n2 == 0)
+            {
+                MessageBox.Show("Division by 0 is not supported",
+                    "Wrong Operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+            return n1 / n2;
+        }
 
     }
 }
